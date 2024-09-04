5549e21ffff41626bbffc6b69f07b659c21dba1d
sunrut-mevawala <sunrutmevawala@gmail.com>
2024-09-04 10:36:21 +0530

    id change cat

---
+++ models/category.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  categoryName: {
+    type: String,
+    required: true,
+  },
+  parentCategoryid: {
+    type: String,
+    default:"",
+  },
+});
+
+const Category = mongoose.model("Categories", categorySchema);
+module.exports = Category;
